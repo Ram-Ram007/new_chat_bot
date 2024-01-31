dd4afd7e78c1b7bedf371962f8097434307a1e94
Ram-Ram007 <ramajayam2411@gmail.com>
2024-01-31 23:46:52 +0530

    modified

--- test.py
+++ test.py
@@ -11,12 +11,12 @@ def load_config(file_path):
         return json.load(file)
 
 def get_response(user_input, intents):
-    user_input = user_input.lower()  # Convert user input to lowercase
+    user_input = user_input.lower()  
     for intent in intents["intents"]:
-        if user_input in map(str.lower, intent["patterns"]):  # Convert patterns to lowercase for comparison
+        if user_input in map(str.lower, intent["patterns"]): 
             return random.choice(intent["responses"])
 
-    # If no response from intents, try web scraping
+
     scraped_response = scrape_website(user_input)
     if scraped_response:
         return scraped_response
@@ -24,14 +24,14 @@ def get_response(user_input, intents):
     return random.choice(["Theriyala Bro", "crt aaa keelu da","Oru alavuku thaa bro"])
 
 def scrape_website(query):
-    # Example: Web scraping code to extract information from a website
+
     url = f'https://www.mediwavedigital.com/q={query}'
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        # Extract information based on HTML structure
-        # For example, extract the text from the first paragraph
+       
+       
         first_paragraph = soup.find('p')
         if first_paragraph:
             return first_paragraph.get_text()
@@ -51,4 +51,4 @@ def get_bot_response():
     return jsonify({'response': bot_response})
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True)
